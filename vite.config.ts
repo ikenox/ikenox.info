96ffed72b55357af378d03da59eb17f912eee85d
naoto.ikeno <ikenox@gmail.com>
2025-02-11 10:16:19 +0900

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,20 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({ plugins: [reactRouter(), tsconfigPaths()] });
+export default defineConfig({
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    {
+      name: 'reload',
+      configureServer(server) {
+        const { ws, watcher } = server;
+        watcher.on('change', (file) => {
+          if (file.endsWith('.md')) {
+            ws.send({ type: 'full-reload' });
+          }
+        });
+      },
+    },
+  ],
+});
