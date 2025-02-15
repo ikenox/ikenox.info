c9ae3165fc62ba6633c1b522c49ae2e3b251a7bf
naoto.ikeno <ikenox@gmail.com>
2025-02-15 11:40:27 +0900

    ogp

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,10 @@ import './app.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const location = useLocation();
+  const canonucalUrl = new URL(
+    location.pathname,
+    'https://ikenox.info'
+  ).toString();
   const isTopPage = location.pathname === '/';
 
   return (
@@ -19,6 +23,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <title>ikenox.info</title>
+
+        <link rel="canonical" href={canonucalUrl} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={canonucalUrl} />
+        <meta property="og:title" content={'ikenox.info'} />
+        <meta property="og:image" content={'/icon.png'} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={canonucalUrl} />
+        <meta property="twitter:title" content={'ikenox.info'} />
+        <meta property="twitter:image" content={'/icon.png'} />
+
         <Meta />
       </head>
       <body>
