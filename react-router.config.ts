362f574151495118ca0e1a9d30ad117b51a48c3e
naoto.ikeno <ikenox@gmail.com>
2025-02-17 23:49:20 +0900

    cloudflare workers

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from '@react-router/dev/config';
 import { getPrerenderPaths } from './app/prerender';
 
-export default { ssr: true, prerender: getPrerenderPaths } satisfies Config;
+export default {
+  ssr: true,
+  // prerender: getPrerenderPaths
+} satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { reactRouter } from '@react-router/dev/vite';
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild ? { input: './workers/app.ts' } : undefined,
+  },
   plugins: [
+    cloudflareDevProxy({
+      getLoadContext({ context }) {
+        return { cloudflare: context.cloudflare };
+      },
+    }),
     reactRouter(),
     {
       name: 'reload',
@@ -16,4 +25,4 @@ export default defineConfig({
       },
     },
   ],
-});
+}));
