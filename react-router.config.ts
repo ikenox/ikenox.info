17cbab3bd420bda7c39155303f9647890238926b
naoto.ikeno <ikenox@gmail.com>
2025-02-18 00:01:24 +0900

    update

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,4 @@
 import type { Config } from '@react-router/dev/config';
 import { getPrerenderPaths } from './app/prerender';
 
-export default {
-  ssr: true,
-  // prerender: getPrerenderPaths
-} satisfies Config;
+export default { ssr: true, prerender: getPrerenderPaths } satisfies Config;
